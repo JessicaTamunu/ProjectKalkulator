6c9cc7383e422486728fadaee939188778013215
JessicaTamunu <jessicatamunu026@student.unsrat.ac.id>
2025-11-25 14:39:47 +0800

    MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -5,7 +5,7 @@ def main():
     print("3. Kali")
     print("4. Bagi")
 
-pilihan = input("Pilih operasi (1-4): "
+pilihan = input("Pilih operasi (1-4): ")
 
 a = float(input("Masukkan angka pertama: "))
 b = float(input("Masukkan angka kedua: "))
